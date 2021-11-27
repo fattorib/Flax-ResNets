d3d18a35c96d88ae4554bf501de00f7008d26e46
Ben <ben.fattorri@mail.utoronto.ca>
2021-11-27 16:30:31 -0500

    github is down LMAO

--- utils_flax.py
+++ utils_flax.py
@@ -4,9 +4,7 @@ import numpy as np
 import jax.numpy as jnp
 import jax
 
-# Utils used for training flax ResNet. Currently holds dataloader class
 
-# From jax example
 def numpy_collate(batch):
     if isinstance(batch[0], np.ndarray):
         return np.stack(batch)
@@ -15,7 +13,6 @@ def numpy_collate(batch):
         return [numpy_collate(samples) for samples in transposed]
     else:
         return np.array(batch)
-
 
 class NumpyLoader(data.DataLoader):
     def __init__(
